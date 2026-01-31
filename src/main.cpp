996ce03fe87afed290c3400b151d77ff0d71e9c1
Thyrocytes <bradenlebron@icloud.com>
2026-01-31 11:50:24 -0600

    alpha changes

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,15 @@ class $modify(Slider) {
 	bool init(cocos2d::CCNode* target, cocos2d::SEL_MenuHandler handler, const char* bar, const char* groove, const char* thumb, const char* thumbSel, float scale) {
 		if (!Slider::init(target, handler, bar, groove, thumb, thumbSel, scale)) return false;
 
-		auto failsafe = geode::WeakRef(this);
-		geode::queueInMainThread([failsafe = std::move(failsafe)] {
-			auto self = failsafe.lock();
-			if (!self) return;
+		this->runAction(geode::cocos::CallFuncExt::create([this] {
+			if (this->getID() == "position-slider") return;
 
-			if (self->getID() == "position-slider") return;
-
-			auto* bar = self->m_sliderBar;
-			bool isScaleControl = geode::cast::typeinfo_cast<GJScaleControl*>(self->getParent());
+			auto* bar = this->m_sliderBar;
+			bool isScaleControl = geode::cast::typeinfo_cast<GJScaleControl*>(this->getParent());
 
 			if (isScaleControl) bar->setPositionY(2.f);
-			bar->setVisible(!geode::Mod::get()->getSettingValue<bool>("opposite-day"));   
-		});
+			bar->setVisible(!geode::Mod::get()->getSettingValue<bool>("opposite-day"));
+		}));
 
 		return true;
 	}
