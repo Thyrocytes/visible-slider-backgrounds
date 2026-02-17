f20c4c4c80c985373f88398f356ed11a4fb2464b
Thyrocytes <reallynewby@icloud.com>
2026-02-16 22:13:13 -0600

    why the hell not

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/Slider.hpp>
 
-class $modify(Slider) {
-	bool init(cocos2d::CCNode* target, cocos2d::SEL_MenuHandler handler, const char* bar, const char* groove, const char* thumb, const char* thumbSel, float scale) {
+struct VisibleBarSlider final : public geode::Modify<VisibleBarSlider, Slider> {
+	bool init(cocos2d::CCNode* target, cocos2d::SEL_MenuHandler handler, char const* bar, char const* groove, char const* thumb, char const* thumbSel, float scale) noexcept {
 		if (!Slider::init(target, handler, bar, groove, thumb, thumbSel, scale)) return false;
 
 		this->runAction(geode::cocos::CallFuncExt::create([this]() noexcept {
