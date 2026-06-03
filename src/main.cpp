447b82e1f4d4735dcf7e6e0e976bfa6a34aefaf4
Thyrocytes <reallynewby@icloud.com>
2026-06-02 20:16:01 -0500

    v1.3.5

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,10 @@
 #include <Geode/modify/Slider.hpp>
 
 struct VisibleBarSlider final : public geode::Modify<VisibleBarSlider, Slider> {
-	bool init(cocos2d::CCNode* target, cocos2d::SEL_MenuHandler handler, char const* bar, char const* groove, char const* thumb, char const* thumbSel, float scale) noexcept {
+	bool init(cocos2d::CCNode* target, cocos2d::SEL_MenuHandler handler, const char* bar, const char* groove, const char* thumb, const char* thumbSel, float scale) noexcept {
 		if (!Slider::init(target, handler, bar, groove, thumb, thumbSel, scale)) return false;
 
-		this->runAction(geode::cocos::CallFuncExt::create([this]() noexcept {
+		this->runAction(geode::cocos::CallFuncExt::create([this] noexcept {
 			if (this->getID() == "position-slider") return;
 			this->updateBar();
 
